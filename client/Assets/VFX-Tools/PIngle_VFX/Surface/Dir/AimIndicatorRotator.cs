865644075737a80b5b4988544030223f9fe3aeb9
Dmytro Tkachuk <d.tkachuk@pinglestudio.com>
2024-06-13 15:09:09 +0300

    Add H4ck misc

--- client/Assets/VFX-Tools/PIngle_VFX/Surface/Dir/AimIndicatorRotator.cs
+++ client/Assets/VFX-Tools/PIngle_VFX/Surface/Dir/AimIndicatorRotator.cs
@@ -6,8 +6,24 @@ public class AimIndicatorRotator : MonoBehaviour
 {
     [SerializeField] private Transform rotation_root = null;
     [SerializeField] private Transform aim_root = null;
+    [SerializeField] private Transform aim_center_root = null;
+
+    [SerializeField] private Transform h4ck_left_dir = null;
+    [SerializeField] private Transform h4ck_mid_dir = null;
+    [SerializeField] private Transform h4ck_right_dir = null;
+    [SerializeField] private float h4ck_angle = 30.0f;
+    
+
+
+    private float scale = 0.0f;
     void Update()
     {
         rotation_root.LookAt(aim_root);
+        scale = Mathf.Max(1.0f, aim_root.localScale.x/2.0f) * 2;
+
+        aim_center_root.localScale = new Vector3(scale,scale,scale);
+
+        h4ck_left_dir. localRotation = Quaternion.Euler( 0.0f, -h4ck_angle/2, 0.0f );
+        h4ck_right_dir.localRotation = Quaternion.Euler( 0.0f, h4ck_angle/2, 0.0f );
     }
 }
