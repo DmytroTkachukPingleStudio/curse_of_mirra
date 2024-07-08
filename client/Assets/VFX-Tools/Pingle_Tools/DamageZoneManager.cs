3be10fd5250ba4e8e2f2dba24c56986bcc47969c
Dmytro Tkachuk <d.tkachuk@pinglestudio.com>
2024-07-08 17:05:33 +0300

    Hotfix

--- client/Assets/VFX-Tools/Pingle_Tools/DamageZoneManager.cs
+++ client/Assets/VFX-Tools/Pingle_Tools/DamageZoneManager.cs
@@ -1,12 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageZoneManager : MonoBehaviour
 {
     [SerializeField] private Renderer dangerZone = null;
     [SerializeField] private Renderer dangerZoneRing = null;
-    [SerializeField] private Renderer dangerZoneShadow = null;
     [SerializeField, Range(0, 0.9f)] private float progress = 0.0f;
     [SerializeField] private float progress_speed = 0.1f;
     [SerializeField] private bool simulate_progress = true;
@@ -37,7 +35,6 @@ public class DamageZoneManager : MonoBehaviour
     private void Update()
     {
         dangerZone.sharedMaterial.SetFloat("_Progress", progress);
-        dangerZoneShadow.sharedMaterial.SetFloat("_Progress", progress);
         dangerZoneRing.transform.localScale = new Vector3( 1-progress, dangerZoneRing.transform.localScale.y, 1-progress );
           
         if (!simulate_progress)
