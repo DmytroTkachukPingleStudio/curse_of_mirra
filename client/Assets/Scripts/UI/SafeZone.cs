c2935803adae5014f3087751466a6410896096b4
saugon <gonnza@gmail.com>
2024-07-11 17:39:30 -0300

    [VFX] Reworked Damage zone effect (#1924)
    * Squashed commits
    
    * Sand storm opt
    
    * Packed assets
    
    * Moved most culc into vertex and disabled shadow
    
    * Cleaned up shaders
    
    * Render queue experiment
    
    * Hotfix
    
    * Cleared shadow  references and test code
    
    * Optimized texture samples count, HDR color usage and added vertex disposition to reduce overdraw
    
    * Adjusted max aplha multiplyer for the damage zone
    
    * Made some visual adjustments to correct the damage zone
    
    ---------
    
    Co-authored-by: Dmytro Tkachuk <d.tkachuk@pinglestudio.com>

--- client/Assets/Scripts/UI/SafeZone.cs
+++ client/Assets/Scripts/UI/SafeZone.cs
@@ -11,27 +11,36 @@ public class SafeZone : MonoBehaviour
     [SerializeField]
     Renderer dangerZone;
 
+    [SerializeField]
+    Renderer dangerZoneRing;
+    float visualCorrectionPercentage = 4f;
+
     void Start()
     {
         dangerZone.sharedMaterial.SetFloat("_AlphaMultiplier", 0f);
+        dangerZoneRing.sharedMaterial.SetFloat("_AlphaMultiplier", 0f);
     }
 
     IEnumerator ShowDangerZone()
     {
         yield return new WaitForSeconds(1f);
+        float parameterValue = 0.0f;
         float currentValue = dangerZone.sharedMaterial.GetFloat("_AlphaMultiplier");
-        if (currentValue == 0.70f)
+        if (currentValue >= 0.85f)
             yield break;
         currentValue += 0.05f;
-        dangerZone
-            .sharedMaterial
-            .SetFloat("_AlphaMultiplier", Mathf.Round(currentValue * 100) / 100f);
+        parameterValue = Mathf.Round(currentValue * 100) / 100f;
+        dangerZone.sharedMaterial.SetFloat("_AlphaMultiplier", parameterValue);
+
+        dangerZoneRing.sharedMaterial.SetFloat("_AlphaMultiplier", parameterValue);
     }
 
     void Update()
     {
         // We have a difference of x100 between the backend and frontend values
         float radius = GameServerConnectionManager.Instance.playableRadius / 100;
+        radius -= ((radius * visualCorrectionPercentage) / 100);
+
         bool isZoneEnabled = GameServerConnectionManager.Instance.zoneEnabled;
 
         if (isZoneEnabled && dangerZone.sharedMaterial.GetFloat("_AlphaMultiplier") != 0.70f)
@@ -44,8 +53,15 @@ public class SafeZone : MonoBehaviour
         double finalVfxValue = 1;
         double initialPlayableRadius = 0.552;
         float currentRadius = radius / 100;
-        double value = finalVfxValue + (currentRadius) * (-finalVfxValue) / (initialPlayableRadius);
+        float value = (float)(
+            finalVfxValue + (currentRadius) * (-finalVfxValue) / (initialPlayableRadius)
+        );
 
-        dangerZone.sharedMaterial.SetFloat("_Progress", (float)value);
+        dangerZone.sharedMaterial.SetFloat("_Progress", value);
+        dangerZoneRing.transform.localScale = new Vector3(
+            1 - value,
+            dangerZoneRing.transform.localScale.y,
+            1 - value
+        );
     }
 }
