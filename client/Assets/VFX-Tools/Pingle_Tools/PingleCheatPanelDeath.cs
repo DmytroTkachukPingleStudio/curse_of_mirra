3117bbf83050264399c98dc51e9be41333496b95
Dmytro Tkachuk <d.tkachuk@pinglestudio.com>
2024-07-19 11:15:30 +0300

    Added cheat for skinnedmesh

--- client/Assets/VFX-Tools/Pingle_Tools/PingleCheatPanelDeath.cs
+++ client/Assets/VFX-Tools/Pingle_Tools/PingleCheatPanelDeath.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PingleCheatPanelDeath : MonoBehaviour
 {
     [SerializeField] private Renderer char_renderer = null;
     [SerializeField] private Transform vfx_root = null;
     [SerializeField] private GameObject death_vfx = null;
+    [SerializeField] private GameObject death_vfx2 = null;
     [SerializeField] private float crystalisation_time = 0.8f;
     [SerializeField] private float crystalisation_offset = 0.5f;
     [SerializeField] private float explosion_time = 1.2f;
+    [SerializeField] private float explosion_offset = 1.2f;
 
     private List<GameObject> pool = new List<GameObject>();
 
@@ -24,6 +27,8 @@ public class PingleCheatPanelDeath : MonoBehaviour
       if (GUI.Button(new Rect( 100, 100, 80, 80 ), "spawn"))
           spawnVFX();
 
+      if (GUI.Button(new Rect( 100, 200, 80, 80 ), "spawn2"))
+          spawnVFX2();
   }
 
   private void spawnVFX()
@@ -47,6 +52,27 @@ public class PingleCheatPanelDeath : MonoBehaviour
         }
 
         char_renderer.material.DOFloat(1, "_ExplosionProgress", explosion_time);
+      }
+  }
+
+  private void spawnVFX2()
+  {
+      StartCoroutine(impl());
+      IEnumerator impl()
+      {
+        char_renderer.material.SetFloat("_DeathProgress", 0);
+        char_renderer.material.SetFloat("_ExplosionProgress", 0);
+        yield return new WaitForSeconds(1.0f);
+
+        yield return new WaitForSeconds(crystalisation_time);
+
+        death_vfx2.GetComponent<VisualEffect>().Play();
+        yield return new WaitForSeconds(explosion_offset);
+
+        char_renderer.material.DOFloat(1, "_ExplosionProgress", explosion_time);
+
+        yield return new WaitForSeconds(crystalisation_offset);
+        char_renderer.material.SetFloat("_ExplosionProgress", 0);
       }
   }
 }
